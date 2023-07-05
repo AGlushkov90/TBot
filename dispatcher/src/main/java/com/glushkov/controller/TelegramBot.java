dccd349ce07c75c66c8887552ce6f14917b565db
Your Name <you@example.com>
2023-07-05 23:08:14 +0300

    init commit

---
+++ dispatcher/src/main/java/com/glushkov/controller/TelegramBot.java
@@ -0,0 +1,59 @@
+package com.glushkov.controller;
+
+import lombok.extern.log4j.Log4j;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+@Log4j
+public class TelegramBot extends TelegramLongPollingBot {
+    @Value("${bot.username}")
+    private String botUserName;
+    @Value("${bot.token}")
+    private String botToken;
+
+    private UpdateController updateController;
+
+    @Autowired
+    public void setUpdateController(UpdateController updateController) {
+        this.updateController = updateController;
+    }
+
+
+    public void init(){
+        updateController.registerBot(this);
+    }
+    @Override
+    public String getBotUsername() {
+        return botUserName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        updateController.processUpdate(update);
+
+
+    }
+
+    public void sendAnswerMessage(SendMessage message) {
+        if (message != null) {
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                log.error(e);
+            }
+        }
+    }
+}
